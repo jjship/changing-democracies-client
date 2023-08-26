5ec0685e110c6d6a19e9e8583ca5ec2953237be9
Jacek Skrzypek <devontheroof@gmail.com>
2023-08-26 17:37:35 +0200

    Teams section sm

--- src/app/content.tsx
+++ src/app/content.tsx
@@ -35,3 +35,180 @@ const projectParagraphSm = (
     democracy today.
   </>
 );
+
+const teamParagraphSm = (
+  <>
+    <b>Changing Democracies</b> involves 13 partners
+    <br />
+    from 10 European countries and is made
+    <br />
+    possible thanks to the funding of the European
+    <br />
+    Commission.
+  </>
+);
+
+export type TeamEntry = {
+  organisation: string;
+  link: string;
+  teamMembers: string[];
+  country: string;
+};
+
+const teamList: TeamEntry[] = [
+  {
+    organisation: "Association for History Education in Greece",
+    link: "https://www.aheg.gr/",
+    teamMembers: ["Marjolein Delvou", "Hanna Zielińska"],
+    country: "Greece",
+  },
+  {
+    organisation: "Autres Directions",
+    link: "https://www.autresdirections.nl/",
+    teamMembers: ["Tijl Akkermans", "Catrien Spijkerman"],
+    country: "The Netherlands",
+  },
+  {
+    organisation: "Borderland Foundation",
+    link: "https://www.pogranicze.sejny.pl/",
+    teamMembers: ["Weronika Czyżewska-Poncyliusz"],
+    country: "Poland",
+  },
+  {
+    organisation:
+      "Escola de Cultura de Pau, Autonomous University of Barcelona",
+    link: "https://escolapau.uab.cat/en/publications",
+    teamMembers: ["Cecile Barbeito-Thonon", "Marina Caireta-Sampere"],
+    country: "Spain",
+  },
+  {
+    organisation: "EuroClio - European Association of History Educators",
+    link: "https://www.euroclio.eu/",
+    teamMembers: ["Eugenie Khatschatrian", "Steven Stegers"],
+    country: "The Netherlands",
+  },
+  {
+    organisation: "Evens Foundation",
+    link: "https://evensfoundation.be/",
+    teamMembers: ["Caroline Coosemans", "Marjolein Delvou", "Hanna Zielińska"],
+    country: "Belgium/Poland",
+  },
+  {
+    organisation: "Flemish Peace Institute",
+    link: "https://vlaamsvredesinstituut.eu/en/",
+    teamMembers: ["Maarten Van Alstein"],
+    country: "Belgium",
+  },
+  {
+    organisation: "In Media Res",
+    link: "",
+    teamMembers: ["Stefano Di Pietro"],
+    country: "The Netherlands",
+  },
+  {
+    organisation: "Institute for the Study of Totalitarian Regimes",
+    link: "https://www.ustrcr.cz/en",
+    teamMembers: ["Bohumil Melichar", "Václav Sixta"],
+    country: "Czechia",
+  },
+  {
+    organisation:
+      "Institute of Research in Art, Design and Society, University of Porto",
+    link: "https://i2ads.up.pt/en/intitute/",
+    teamMembers: ["Samuel Guimaraes", "Catarina Silva Martins"],
+    country: "Portugal",
+  },
+  {
+    organisation: "Open Lithuania Foundation",
+    link: "https://olf.lt/en/",
+    teamMembers: ["Sandra Adomavičiūtė", "Sandra Gaučiūtė"],
+    country: "Lithuania",
+  },
+  {
+    organisation: "Mediawise Society",
+    link: "https://mediawise.ro/",
+    teamMembers: ["Nicoleta Fotiade", "Nicoleta Neacsu"],
+    country: "Romania",
+  },
+  {
+    organisation: "National History Museum of Slavonia",
+    link: "https://www.mso.hr/",
+    teamMembers: ["Denis Detling", "Jesenka Ricl", "Vasiliki Sakka"],
+    country: "Croatia",
+  },
+];
+
+type FutureEventEntry = {
+  isPast: boolean;
+  date: string;
+  title: string;
+  kind: string;
+  location: string;
+  link: string;
+};
+
+type PastEventEntry = FutureEventEntry & {
+  isPast: true;
+  participants: number;
+  wp3: string;
+};
+
+const upcomingEvents: FutureEventEntry[] = [
+  {
+    isPast: false,
+    date: "10.12.23",
+    title: "Come and find out",
+    kind: "Meeting | online",
+    location: "Slavonia, Croatia",
+    link: "",
+  },
+  {
+    isPast: false,
+    date: "10.12.23",
+    title: "Come and find out",
+    kind: "Meeting | online",
+    location: "Slavonia, Croatia",
+    link: "",
+  },
+  {
+    isPast: false,
+    date: "10.12.23",
+    title: "Come and find out",
+    kind: "Meeting | online",
+    location: "Slavonia, Croatia",
+    link: "",
+  },
+];
+
+const pastEvents: PastEventEntry[] = [
+  {
+    isPast: true,
+    date: "20 - 22.04.23",
+    title: "Are we lost?",
+    kind: "Workshop | online",
+    location: "Sejny, Poland",
+    link: "",
+    participants: 50,
+    wp3: "WP3",
+  },
+  {
+    isPast: true,
+    date: "20 - 22.04.23",
+    title: "Are we lost?",
+    kind: "Workshop | online",
+    location: "Sejny, Poland",
+    link: "",
+    participants: 50,
+    wp3: "WP3",
+  },
+  {
+    isPast: true,
+    date: "20 - 22.04.23",
+    title: "Are we lost?",
+    kind: "Workshop | online",
+    location: "Sejny, Poland",
+    link: "",
+    participants: 50,
+    wp3: "WP3",
+  },
+];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,12 @@ import Image from "next/image";
 import earshapedArrows from "../../public/arrows_ear_no_bg.svg";
 import logoLight from "../../public/logo_light_no_bg.svg";
 import fundedByEu from "../../public/EU_flag__funded_by.svg";
-import { TeamEntry, projectParagraphSm } from "./content";
+import {
+  TeamEntry,
+  projectParagraphSm,
+  teamParagraphSm,
+  teamList,
+} from "./content";
 
 export default function Page() {
   return (
@@ -26,9 +31,18 @@ export default function Page() {
           </div>
         </div>
       </Section>
-      <Section id="team" styling="bg-black_bg text-puprple_lightest_bg">
+      <Section
+        id="team"
+        styling="bg-black_bg text-puprple_lightest_bg px-[1.25em] pb-[2.5em]"
+      >
         <Title text="Team" />
+        <div className="flex flex-col items-center gap-[1.125em]">
+          <Paragraph text={teamParagraphSm} />
+          <TeamsList teams={teamList} />
+        </div>
       </Section>
+      <section id="events">events section</section>
+      <section id="contact">contact section</section>
     </main>
   );
 }
@@ -46,20 +60,68 @@ function Section(props: {
 }
 
 function Paragraph(props: { text: string | JSX.Element }) {
-  return (
-    <div className="whitespace-pre-line leading-5">
-      <p>{props.text}</p>
-    </div>
-  );
+  return <p className="leading-5 ">{props.text}</p>;
 }
 
 function Title(props: { text: string }) {
   return (
-    <div className="w-full pl-[2.5em]  pt-[1.875em]">
-      <h1 className="text-[2.25rem] leading-9 tracking-[-0.064rem] py-[0.3em]">
+    <div className="w-full pl-[1.5em]  pt-[1.875em] pb-[1.25em]">
+      <h1 className="text-[2.25rem] font-semibold leading-9 tracking-[-0.064rem] py-[0.3em]">
         {props.text}
       </h1>
     </div>
   );
 }
+
+function TeamsList(props: { teams: TeamEntry[] }) {
+  return (
+    <div className="flex flex-col items-left gap-[.938em]">
+      {props.teams.map((team) => (
+        <TeamEntryComponent key={team.organisation} {...team} />
+      ))}
+    </div>
+  );
+}
+
+function TeamEntryComponent(props: TeamEntry) {
+  return (
+    <div>
+      <Link className="leading-4" href={props.link}>
+        {props.organisation}
+      </Link>
+      <p className="text-[.875rem] leading-[.875rem] text-yellow_secondary ">
+        {props.teamMembers.join(", ")}
+      </p>
+
+      <p className="leading-[1.125em] px-[.313em] text-black_bg bg-red_mains max-w-fit">
+        <svg
+          className="inline-block align-[.125em] mr-[.313em] fill-black_bg"
+          xmlns="http://www.w3.org/2000/svg"
+          width="14"
+          height="6"
+          viewBox="0 0 14 6"
+          fill="none"
+        >
+          <title>arrow</title>
+          <path d="M14 3L9 0.113249V5.88675L14 3ZM0 3.5H9.5V2.5H0V3.5Z" />
+        </svg>
+        {props.country}
+      </p>
+    </div>
+  );
+}
+
+function NavBar() {
+  return <div></div>;
+}
+
+function Navigation() {
+  return (
+    <nav className="flex flex-col items-left justify-between">
+      <Link href="#project">Project</Link>
+      <Link href="#team">Team</Link>
+      <Link href="#events">Events</Link>
+      <Link href="#contact">Contact</Link>
+    </nav>
+  );
 }
