c4620381db69af22a53122bb18f609c086382288
Jacek Skrzypek <devontheroof@gmail.com>
2023-08-26 12:45:23 +0200

    Project section sm

---
+++ src/app/content.tsx
@@ -0,0 +1,37 @@
+export {
+  projectFirstParagraphBg,
+  projectSecondParagraphBg,
+  projectParagraphSm,
+  teamParagraphSm,
+  teamList,
+  upcomingEvents,
+  pastEvents,
+};
+
+const projectFirstParagraphBg =
+  "What would life be like without democracy?<br/>Has democracy lived up to its promises?\nWhat does a healthy democracy need to\nfunction effectively? What is the role of\nitizens?";
+
+const projectSecondParagraphBg =
+  "<b>Changing Democracies</b> aims to collect\nuntold stories from across Europe about\nrecent transitions to democracy and explore\nhow they can lead to critical reflection and\ndialogue about democracy today.";
+
+const projectParagraphSm = (
+  <>
+    What would life be like without democracy? Has
+    <br />
+    democracy lived up to its promises? What does
+    <br />a healthy democracy need to function
+    <br />
+    effectively? What is the role of citizens
+    <br />
+    <br />
+    <b>Changing Democracies</b> aims to collect untold
+    <br />
+    stories from across Europe about recent
+    <br />
+    transitions to democracy and explore how they
+    <br />
+    can lead to critical reflection and dialogue about
+    <br />
+    democracy today.
+  </>
+);
