fb917bfca0821ef7fdaf51f390b0eb2f32f80d6e
Jacek Skrzypek <devontheroof@gmail.com>
2023-08-31 21:09:31 +0200

    add tesktop media query

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -35,10 +35,8 @@ const config: Config = {
     },
 
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      screens: {
+        dt: "1440px",
       },
     },
   },
