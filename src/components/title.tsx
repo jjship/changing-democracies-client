ca52692daada5bb16b1f569cb4bc4c8f6a742ad5
Jacek Skrzypek <devontheroof@gmail.com>
2023-08-31 19:51:21 +0200

    fix title padding

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -7,7 +7,7 @@ export default function Title(props: {
 
   return (
     <div className={`${bgColor} sticky top-16 z-30 `}>
-      <h1 className="py-[0.3em] pl-6 text-[2.25rem] font-semibold leading-9 tracking-[-0.064rem]">
+      <h1 className="py-[0.3em] pl-5 text-[2.25rem] font-semibold leading-9 tracking-[-0.064rem]">
         {props.text}
       </h1>
     </div>
