112d2a519e971df89d1fff9211633a328f54d496
Jacek Skrzypek <devontheroof@gmail.com>
2023-09-04 20:25:55 +0200

    prettify classnames order

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -7,9 +7,9 @@ export default function Title(props: {
 
   return (
     <div
-      className={`${bgColor} dt:relative dt:top-0 dt:z-20 sticky top-16 z-30 `}
+      className={`${bgColor} sticky top-16 z-30 dt:relative dt:top-0 dt:z-20 `}
     >
-      <h1 className="dt:text-[4.5rem] dt:font-bold dt:pl-16 dt:py-10 dt:leading-8 py-[0.3em] pl-5 text-[2.25rem] font-semibold leading-9 tracking-[-0.064rem]">
+      <h1 className="py-[0.3em] pl-5 text-[2.25rem] font-semibold leading-9 tracking-[-0.064rem] dt:py-10 dt:pl-16 dt:text-[4.5rem] dt:font-bold dt:leading-8">
         {props.text}
       </h1>
     </div>
