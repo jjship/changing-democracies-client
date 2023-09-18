c2e5be982dd1487d5f6d65450b5d1b74e365e884
Jacek Skrzypek <devontheroof@gmail.com>
2023-09-18 19:13:02 +0200

    add password reset

---
+++ app/password-reset/page.tsx
@@ -0,0 +1,52 @@
+import Messages from "../login/messages";
+import { cookies } from "next/headers";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+
+const supabase = createServerComponentClient({ cookies });
+
+export default async function ResetPassword() {
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  return (
+    <div className="flex min-h-screen flex-col items-center bg-puprple_lightest_bg">
+      <div className="flex w-full flex-1 flex-col justify-center gap-2 px-8 sm:max-w-md">
+        <form
+          className="flex w-full flex-1 flex-col justify-center gap-2 text-black_bg"
+          action="/auth/reset-password"
+          method="post"
+        >
+          {user?.email && (
+            <>
+              <label className="text-md" htmlFor="user_email">
+                Email
+              </label>
+              <input
+                className="mb-6 rounded-md border bg-inherit px-4 py-2"
+                type="email"
+                name="email"
+                value={user.email}
+                disabled
+              />
+            </>
+          )}
+          <label className="text-md" htmlFor="new_password">
+            New password
+          </label>
+          <input
+            className="mb-6 rounded-md border bg-white px-4 py-2"
+            type="password"
+            name="password"
+            placeholder="••••••••"
+            required
+          />
+          <button className="hiver:text-black_bg mb-2 rounded bg-green_accent px-4 py-2 text-black_bg  hover:bg-yellow_secondary">
+            Update Password & Log In` `
+          </button>
+          <Messages />
+        </form>
+      </div>
+    </div>
+  );
+}
---
+++ components/PasswordResetForm.tsx
@@ -0,0 +1,102 @@
+"use client";
+import * as z from "zod";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { Database } from "../types/database";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "./ui/button";
+
+const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
+
+const formSchema = z.object({
+  email: z.string().email({ message: "Must be a valid email address" }),
+});
+
+type FormValues = z.infer<typeof formSchema>;
+
+export default function PasswordResetForm() {
+  const [email] = useState("");
+  const [submited, setSubmited] = useState<boolean>(true);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const supabase = createClientComponentClient<Database>();
+
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: { email },
+    mode: "onChange",
+  });
+
+  async function onSubmit(values: FormValues) {
+    console.log({ values });
+    const { data, error } = await supabase.auth.resetPasswordForEmail(
+      values.email,
+      {
+        redirectTo: `${baseURL}/auth/callback?next=/password-reset`,
+      },
+    );
+    if (error) {
+      console.error(error);
+    }
+    setSubmited(true);
+  }
+
+  if (!isOpen) {
+    return (
+      <Button
+        className="text-md w-full bg-yellow_secondary  text-black_bg hover:bg-green_accent"
+        onClick={() => setIsOpen(true)}
+      >
+        Reset Password
+      </Button>
+    );
+  }
+
+  return !submited ? (
+    <div className=" flex w-full flex-1 flex-col justify-center gap-2 sm:max-w-md">
+      <Form {...form}>
+        <form
+          className="text-md flex w-full flex-1 flex-col justify-center gap-2 text-black_bg"
+          onSubmit={form.handleSubmit(onSubmit)}
+        >
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-md">Email</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button
+            className="text-md justify-self-center bg-yellow_secondary py-2 text-black_bg hover:bg-green_accent"
+            type="submit"
+          >
+            Send password reset email
+          </Button>
+        </form>
+      </Form>
+    </div>
+  ) : (
+    <>
+      <p className="text-center">
+        We have sent you an email with a link to reset your password.
+      </p>
+      <p className="text-center">check your SPAM folder</p>
+    </>
+  );
+}
