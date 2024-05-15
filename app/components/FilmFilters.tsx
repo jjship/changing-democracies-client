a9ccaf7453591f204d0fe441afa69d8605b31a73
Jacek Skrzypek <devontheroof@gmail.com>
2024-05-15 21:13:00 +0200

    fix lint errors

--- app/components/FilmFilters.tsx
+++ app/components/FilmFilters.tsx
@@ -14,8 +14,11 @@ const Filters: React.FC<FilmListProps> = ({ films }) => {
 
   return (
     <div className="mb-4 flex space-x-2">
-      {tags.map((tag) => (
-        <button className="rounded bg-green_accent px-4 py-2 text-black transition-colors hover:bg-yellow_secondary">
+      {tags.map((tag, i) => (
+        <button
+          key={i}
+          className="rounded bg-green_accent px-4 py-2 text-black transition-colors hover:bg-yellow_secondary"
+        >
           {tag}
         </button>
       ))}
