5cea51a2ddcb34decf25b626790b3f1ed588eba5
Jacek Skrzypek <devontheroof@gmail.com>
2024-05-15 22:04:39 +0200

    fixes

--- app/components/FilmList.tsx
+++ app/components/FilmList.tsx
@@ -13,18 +13,19 @@ interface FilmListProps {
 
 const FilmList: React.FC<FilmListProps> = ({ films }) => {
   return (
-    <div className="space-y-4">
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
       {films.map((film) => (
         <Link key={film.guid} href={`/film/${film.guid}`}>
           <div className="cursor-pointer rounded border border-gray-300 p-4 transition-colors hover:bg-gray-200">
             <Image
               src={getThumbnail(film.guid)}
-              alt="link to play film"
+              alt={film.title}
               width={300}
               height={200}
-            ></Image>
-            <h3 className="text-xl font-semibold">{film.title}</h3>
-            <p className="text-gray-600">
+              className="h-auto w-full rounded"
+            />
+            <h3 className="mt-2 text-xl font-semibold">{film.title}</h3>
+            <p className="mt-1 text-gray-600">
               {film.metaTags
                 .find((tag) => tag.property === "tags")
                 ?.value?.split(",")
