f638f4c54153932f7d97c0951aef5a57e4b9adeb
Jacek Skrzypek <devontheroof@gmail.com>
2024-05-16 12:33:38 +0200

    wip

--- app/admin/photobooth/posters/page.tsx
+++ app/admin/photobooth/posters/page.tsx
@@ -31,3 +31,37 @@ export default async function PostersPage() {
     </div>
   );
 }
+
+async function getPosters(): Promise<{
+  success: boolean;
+  data: BunnyPoster[];
+  error?: { message: string; status: number };
+}> {
+  if (!process.env.BUNNY_STORAGE_API_KEY) {
+    throw new Error("Missing Bunny Stream environment variables");
+  }
+
+  const url = "https://storage.bunnycdn.com/cd-dev-storage/posters/";
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      AccessKey: process.env.BUNNY_STORAGE_API_KEY,
+    },
+  };
+
+  const res = await fetch(url, options);
+
+  if (!res.ok) {
+    return {
+      success: false,
+      data: [],
+      error: { message: "Failed to fetch posters data", status: res.status },
+    };
+  }
+
+  const posters: BunnyPoster[] = (await res.json()) as BunnyPoster[];
+
+  return { success: true, data: posters };
+}
