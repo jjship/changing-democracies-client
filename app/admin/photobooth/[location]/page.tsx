3d9f298a8eca7014710a603e67563d78939a9b5d
Jacek Skrzypek <devontheroof@gmail.com>
2024-06-02 22:27:28 +0200

    fix errors

--- app/admin/photobooth/[location]/page.tsx
+++ app/admin/photobooth/[location]/page.tsx
@@ -1,16 +1,13 @@
 "use client";
 
-import { useState } from "react";
 import LocationForm from "../../../components/admin/photobooth/LocationForm";
 import Photobooth from "../../../components/admin/photobooth/Photobooth";
 
-export default async function PhotoboothPage({
+export default function PhotoboothPage({
   params: { location },
 }: {
   params: { location?: string };
 }) {
-  const [showBooth, setShowBoot] = useState(false);
-
   return (
     <>{location ? <Photobooth location={location} /> : <LocationForm />}</>
   );
