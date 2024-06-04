43586cc07b0ae0990250fdef06c5c698e78045a1
Jacek Skrzypek <devontheroof@gmail.com>
2024-06-04 19:44:41 +0200

    fix error message

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -1,6 +1,6 @@
 export default function ErrorPage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 text-red_mains">
       Sorry, something went wrong
     </main>
   );
