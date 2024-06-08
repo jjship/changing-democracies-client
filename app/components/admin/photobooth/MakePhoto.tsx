985b8d92cc06bea7d42f7d3d9b9f601203f47754
Jacek Skrzypek <devontheroof@gmail.com>
2024-06-08 12:35:39 +0200

    wip

--- app/components/admin/photobooth/MakePhoto.tsx
+++ app/components/admin/photobooth/MakePhoto.tsx
@@ -32,28 +32,81 @@ const VideoWithFilters = () => {
     frameRate: 20,
   };
 
+  const applyFilter = (
+    context: CanvasRenderingContext2D | null,
+    width: number,
+    height: number,
+  ) => {
+    if (!context) return;
+    const imageData = context.getImageData(0, 0, width, height);
+    const data = imageData.data;
+
+    for (let i = 0; i < data.length; i += 4) {
+      const r = data[i];
+      const g = data[i + 1];
+      const b = data[i + 2];
+      const brightnessValue = (r + g + b) / 3;
+
+      const t = brightnessValue / 255;
+      const interColor = {
+        r: 64 * (1 - t) + 224 * t,
+        g: 224 * (1 - t) + 208 * t,
+        b: 208 * (1 - t) + 208 * t,
+      };
+
+      data[i] = interColor.r;
+      data[i + 1] = interColor.g;
+      data[i + 2] = interColor.b;
+    }
+
+    context.putImageData(imageData, 0, 0);
+  };
+
   const getScreenshot = useCallback(() => {
     if (canvasRef.current) {
-      canvasRef.current.toBlob(async (blob) => {
-        const filename = "poster_" + uuidv4() + "_" + location + ".jpeg";
-        setFilename(filename);
-        if (blob) {
-          const formData = new FormData();
-          formData.append("blob", blob);
-          formData.append("fileName", filename);
+      const targetCanvas = canvasRef.current;
+      const targetContext = targetCanvas.getContext("2d");
 
-          const result = await saveImage(formData);
-          if (result.success) {
-            console.log("Image uploaded successfully");
-          } else {
-            console.error("Failed to upload image");
-          }
+      if (webcamRef.current) {
+        const sourceCanvas = webcamRef.current.getCanvas();
+
+        if (sourceCanvas) {
+          targetCanvas.width = windowWidth;
+          targetCanvas.height = windowHeight * 0.9;
+
+          targetContext?.drawImage(
+            sourceCanvas,
+            0,
+            0,
+            windowWidth,
+            windowHeight * 0.9,
+          );
+
+          // Apply the filter before capturing the image
+          applyFilter(targetContext, windowWidth, windowHeight * 0.9);
+
+          targetCanvas.toBlob(async (blob) => {
+            const filename = "poster_" + uuidv4() + "_" + location + ".jpeg";
+            setFilename(filename);
+            if (blob) {
+              const formData = new FormData();
+              formData.append("blob", blob);
+              formData.append("fileName", filename);
+
+              const result = await saveImage(formData);
+              if (result.success) {
+                console.log("Image uploaded successfully");
+              } else {
+                console.error("Failed to upload image");
+              }
+            }
+          }, "image/jpeg");
+
+          setStage(4);
         }
-      }, "image/jpeg");
-
-      setStage(4);
+      }
     }
-  }, [canvasRef, location, setStage]);
+  }, [canvasRef, location, setStage, windowWidth, windowHeight]);
 
   const capture = useCallback(() => {
     const webcam = webcamRef.current;
