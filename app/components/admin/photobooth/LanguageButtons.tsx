d525cca08a4a11caf40eadeb4051c6c6fdac1615
Jacek Skrzypek <devontheroof@gmail.com>
2024-06-17 19:08:08 +0200

    add choose your language

--- app/components/admin/photobooth/LanguageButtons.tsx
+++ app/components/admin/photobooth/LanguageButtons.tsx
@@ -8,20 +8,22 @@ import BackBtn from "./BackBtn";
 
 const LanguageButtons: FC = () => {
   const { windowHeight, stage } = useBoothContext();
-
   const width = 120;
   const height = 50;
   const buttonSpacing = 30;
   const startY =
     (windowHeight -
       (height * languages.length + buttonSpacing * (languages.length - 1))) /
-    2;
+    3;
 
   if (stage !== -1) return null;
 
   return (
     <>
       <BackBtn />
+
+      <p className="mt-12 pb-0 text-[24px]">Choose your language</p>
+
       {languages.map((lang, i) => {
         const btnY = startY + i * buttonSpacing;
         const props = {
