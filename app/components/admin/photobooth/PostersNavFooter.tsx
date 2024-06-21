f406612553c8969d262cfec2db7baa46bfe8f2f6
Jacek Skrzypek <devontheroof@gmail.com>
2024-06-21 17:49:40 +0200

    handle filter grid

--- app/components/admin/photobooth/PostersNavFooter.tsx
+++ app/components/admin/photobooth/PostersNavFooter.tsx
@@ -2,10 +2,7 @@ import { FC, MouseEventHandler, useCallback } from "react";
 import Image from "next/image";
 
 import simple_arrow from "@/public/simple_arrow.svg";
-
 import { Button } from "@/ui/button";
-import { boothBtn } from "../photobooth/boothConstats";
-
 import { useTranslations } from "./useTranslations";
 
 type PostersNavFooterProps = {
@@ -24,46 +21,37 @@ const PostersNavFooter: FC<PostersNavFooterProps> = ({
   const { make } = useTranslations();
   const totalButtons = locations.length;
   const isSingleRow = totalButtons <= 6;
-  const getGridClass = useCallback(
-    (index: number) => {
-      const isLastRow =
-        Math.ceil((index + 1) / 7) === Math.ceil(totalButtons / 7);
 
-      if (isSingleRow || (isLastRow && totalButtons % 7 <= 6)) {
-        return "grid-cols-6-cols";
-      }
-
-      return "grid-cols-7-cols";
-    },
-    [locations, isSingleRow],
-  );
+  const buttonsArray = useCallback(() => {
+    const buttons = locations.map((posterLocation) => (
+      <Button
+        key={posterLocation}
+        value={posterLocation}
+        className={`${
+          selectedLocation === posterLocation || !selectedLocation
+            ? "bg-green_accent"
+            : "bg-gray_light_secondary"
+        } font-black  text-black hover:bg-yellow_secondary`}
+        onClick={handleFilterClick}
+      >
+        {posterLocation}
+      </Button>
+    ));
+    if (locations.length > 7 && locations.length % 7 === 0) {
+      buttons.push(<Button key="invisible_btn" className="invisible"></Button>);
+    }
+    return buttons;
+  }, [locations, selectedLocation]);
 
   return (
     <div className="bg-purple fixed bottom-0 max-h-min w-full flex-col px-20 pt-10 font-black">
       {locations && (
-        <div
-          className={`grid min-h-max gap-x-7 gap-y-5 ${getGridClass(
-            totalButtons - 1,
-          )}`}
-        >
-          {Array.from(locations).map((posterLocation) => (
-            <Button
-              key={posterLocation}
-              value={posterLocation}
-              className={`${
-                selectedLocation === posterLocation || !selectedLocation
-                  ? "bg-green_accent"
-                  : "bg-gray_light_secondary"
-              } font-black  text-black hover:bg-yellow_secondary`}
-              onClick={handleFilterClick}
-            >
-              {posterLocation}
-            </Button>
-          ))}
+        <div className={`grid-cols-7-cols grid min-h-max gap-x-7 gap-y-5`}>
+          {buttonsArray()}
         </div>
       )}
       <div className="mb-5 flex h-36 w-full justify-between ">
-        <p className="mt-9 text-5xl font-extrabold text-white">
+        <p className="mt-9 min-w-min text-5xl font-extrabold text-white">
           {make.toUpperCase()}
         </p>
         <Image src={simple_arrow} alt="arrow" className="mb-6" />
