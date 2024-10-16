b6094afd76cac69257c9542949ec42c3fced1b1b
Jacek Skrzypek <devontheroof@gmail.com>
2024-10-16 18:47:46 +0200

    set title font to archivo_black

--- app/components/Title.tsx
+++ app/components/Title.tsx
@@ -1,3 +1,7 @@
+import { Archivo_Black } from "next/font/google";
+
+const archivo_black = Archivo_Black({ weight: "400", subsets: ["latin"] });
+
 export default function Title(props: {
   text: string;
   theme: "light" | "dark";
@@ -11,7 +15,9 @@ export default function Title(props: {
       className={`${bgColor} sticky top-10 z-30 md:relative md:top-0 md:z-20 `}
     >
       <h1
-        className={`py-[0.3em] pl-5 text-[2.25rem] font-semibold leading-9 tracking-[-0.064rem] md:py-10 md:pl-12 md:text-7xl md:font-bold md:leading-8 xl:pl-16 xl:text-[4.5rem] ${
+        className={`${
+          archivo_black.className
+        } py-[0.3em] pl-5 text-[2.25rem] leading-9 tracking-[-0.064rem] md:py-10 md:pl-12 md:text-7xl md:leading-8 xl:pl-24 xl:text-[4.5rem] ${
           color ? "text-" + color : ""
         }`}
       >
