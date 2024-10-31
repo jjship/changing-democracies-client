46965e80ac9c5f8903c46f5b71e04e2172715093
Jacek Skrzypek <devontheroof@gmail.com>
2024-10-31 12:13:15 +0100

    alt title

--- app/components/Title.tsx
+++ app/components/Title.tsx
@@ -6,22 +6,23 @@ export default function Title(props: {
   text: string;
   theme: "light" | "dark";
   color?: string;
+  alt?: boolean;
 }) {
-  const { theme, color } = props;
+  const { text, theme, color, alt } = props;
   const bgColor = theme === "light" ? "bg-purple_lightest_bg" : "bg-black_bg";
+  const fontClass = alt ? archivo_black.className : undefined;
+  const topDistance = alt ? null : "top-10";
 
   return (
     <div
-      className={`${bgColor} sticky top-10 z-30 md:relative md:top-0 md:z-20 `}
+      className={`${bgColor} sticky ${topDistance} z-30 md:relative md:top-0 md:z-20 `}
     >
       <h1
-        className={`${
-          archivo_black.className
-        } py-[0.3em] pl-5 text-[2.25rem] leading-9 tracking-[-0.064rem] md:py-10 md:pl-12 md:text-7xl md:leading-8 xl:pl-24 xl:text-[4.5rem] ${
+        className={`${fontClass} py-[0.3em] pl-5 text-[2.25rem] leading-9 tracking-[-0.064rem] md:py-10 md:pl-12 md:text-7xl md:leading-8 xl:pl-24 xl:text-[4.5rem] ${
           color ? "text-" + color : ""
         }`}
       >
-        {props.text}
+        {text}
       </h1>
     </div>
   );
