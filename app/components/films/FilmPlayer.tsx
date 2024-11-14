ddaf3bef24b7d86ea4520cbaa465e81418a0fb48
Jacek Skrzypek <devontheroof@gmail.com>
2024-11-14 08:03:30 +0100

    use player.js and ref to handle playback events

--- app/components/films/FilmPlayer.tsx
+++ app/components/films/FilmPlayer.tsx
@@ -1,25 +1,26 @@
+import { useEffect, useRef } from "react";
+import { Player } from "player.js";
 import CloseButton from "../../components/films/CloseButton";
 import { useFilmsContext } from "./FilmsContext";
-import { useEffect } from "react";
 
 export default function FilmPlayer(props: { onEnded?: () => void }) {
   const { nowPlaying } = useFilmsContext();
   const { onEnded } = props;
   const src = `https://iframe.mediadelivery.net/embed/${process.env.NEXT_PUBLIC_LIBRARY_ID}/${nowPlaying}?autoplay=true&captions=EN`;
+  const iframeRef = useRef(null);
 
   useEffect(() => {
-    const handleMessage = (event: MessageEvent) => {
-      if (event.origin !== "https://iframe.mediadelivery.net") return;
-      if (event.data.event === "videoEnded") {
-        console.log("FilmPlayer: Video ended, calling onEnded");
-        if (onEnded) {
-          onEnded();
-        }
-      }
-    };
+    if (iframeRef.current && onEnded) {
+      const player = new Player(iframeRef.current);
 
-    window.addEventListener("message", handleMessage);
-    return () => window.removeEventListener("message", handleMessage);
+      player.on("ready", () => {
+        player.on("ended", () => {
+          onEnded();
+        });
+
+        player.play();
+      });
+    }
   }, [onEnded]);
 
   return (
@@ -29,7 +30,9 @@ export default function FilmPlayer(props: { onEnded?: () => void }) {
         className="absolute left-0 top-0 z-50 h-full w-full bg-black_bg"
       >
         <CloseButton />
+
         <iframe
+          ref={iframeRef}
           src={src}
           className="absolute left-0 top-0 h-full w-full"
           allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
