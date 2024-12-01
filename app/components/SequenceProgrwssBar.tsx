365472441f6afaeab6ab182ea312e7712dcc0652
ImperatorZurg <blehableha111@gmail.com>
2024-12-01 18:18:10 +0100

    sequence component

--- app/components/SequenceProgrwssBar.tsx
+++ app/components/SequenceProgrwssBar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { memo } from "react";
+import React, { memo, useState } from "react";
 import { Box, Flex } from "@radix-ui/themes";
 import { narrationPath } from "@/app/narrations/firstPath";
 
@@ -15,6 +15,7 @@ const SequenceProgressBar = memo(
     currentFragmentIndex,
     onFragmentSelect,
   }: SequenceProgressBarProps) => {
+    const [isHovered, setIsHovered] = useState<number | null>(null);
     const dotSize = 16; // Zwiększona wielkość kropek
     const lineThickness = 6; // Zwiększona grubość linii
 
@@ -37,66 +38,72 @@ const SequenceProgressBar = memo(
         }}
       >
         {Array.from({ length: totalFragments }).map((_, index) => (
-          <>
-            <Flex
-              key={index}
-              style={{
-                flex: 1,
-                alignItems: "center",
-                position: "relative",
-              }}
-            >
-              {index < totalFragments - 1 && (
-                <Box
-                  style={{
-                    height: `${lineThickness}px`,
-                    width: `calc(100% + ${dotSize}px)`,
-                    position: "absolute",
-                    top: `${dotSize / 2 - lineThickness / 2}px`, // Wyśrodkowanie linii
-                    left: `calc(-${dotSize / 50}px)`, // Wyrównanie linii względem środka kropek
-                    backgroundColor:
-                      index < currentFragmentIndex ? "#808881" : "transparent",
-                    transition: "background-color 0.3s ease",
-                    zIndex: 0, // Linie za kropkami
-                  }}
-                />
-              )}
-              <Flex direction={"column"} align={"center"}>
-                <Box
-                  style={{
-                    backgroundImage: `url( ${narrationPath.fragments[index].thumbnailUrl} )`,
-                    backgroundSize: "cover",
-                    backgroundPosition: "center",
-                  }}
-                  className={`absolute z-20 h-[5vh] w-[5vh] rounded-full border-2 border-[#cf9855] text-white`}
-                ></Box>
-                <Box className={"mt-4 h-12 w-[2px] bg-[#cf9855]"} />
-              </Flex>
+          <Flex
+            key={index}
+            style={{
+              flex: 1,
+              alignItems: "center",
+              position: "relative",
+            }}
+          >
+            {index < totalFragments - 1 && (
               <Box
                 style={{
-                  width:
-                    index === currentFragmentIndex
-                      ? `${dotSize * 1.3}px`
-                      : `${dotSize}px`,
-                  height:
-                    index === currentFragmentIndex
-                      ? `${dotSize * 1.3}px`
-                      : `${dotSize}px`,
-                  borderRadius: "50%",
+                  height: `${lineThickness}px`,
+                  width: `calc(100% + ${dotSize}px)`,
+                  position: "absolute",
+                  top: `${dotSize / 2 - lineThickness / 2}px`, // Wyśrodkowanie linii
+                  left: `calc(-${dotSize / 50}px)`, // Wyrównanie linii względem środka kropek
                   backgroundColor:
-                    index === currentFragmentIndex
-                      ? "#6bdbc6ff"
-                      : index < currentFragmentIndex
-                      ? "#b85252"
-                      : "#808881",
+                    index < currentFragmentIndex ? "#808881" : "transparent",
                   transition: "background-color 0.3s ease",
-                  cursor: "pointer",
-                  zIndex: 1, // Kropki przed liniami
+                  zIndex: 0, // Linie za kropkami
                 }}
-                onClick={() => onFragmentSelect(index)}
               />
+            )}
+            <Flex direction={"column"} align={"center"}>
+              {isHovered === index && (
+                <>
+                  <Box
+                    style={{
+                      backgroundImage: `url(${narrationPath.fragments[index].thumbnailUrl})`,
+                      backgroundSize: "cover",
+                      backgroundPosition: "center",
+                      top: "-6vh",
+                      position: "absolute",
+                    }}
+                    className={`z-20 h-[8vh] w-[8vh] rounded-full border-2 border-[#cf9855] text-white`}
+                  ></Box>
+                  <Box className={"mt-4 h-12 w-[2px] bg-[#cf9855]"} />
+                </>
+              )}
             </Flex>
-          </>
+            <Box
+              style={{
+                width:
+                  index === currentFragmentIndex
+                    ? `${dotSize * 1.3}px`
+                    : `${dotSize}px`,
+                height:
+                  index === currentFragmentIndex
+                    ? `${dotSize * 1.3}px`
+                    : `${dotSize}px`,
+                borderRadius: "50%",
+                backgroundColor:
+                  index === currentFragmentIndex
+                    ? "#6bdbc6ff"
+                    : index < currentFragmentIndex
+                    ? "#b85252"
+                    : "#808881",
+                transition: "background-color 0.3s ease",
+                cursor: "pointer",
+                zIndex: 1, // Kropki przed liniami
+              }}
+              onClick={() => onFragmentSelect(index)}
+              onMouseEnter={() => setIsHovered(index)}
+              onMouseLeave={() => setIsHovered(null)}
+            />
+          </Flex>
         ))}
       </Flex>
     );
