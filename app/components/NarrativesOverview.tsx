9d249aa21f341e3b18b76ca23dea8304da0beddd
ImperatorZurg <blehableha111@gmail.com>
2024-12-13 13:23:57 +0100

    fix: remove narrationView from page and create NarrativesOverview component

---
+++ app/components/NarrativesOverview.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import React, { FC, useState } from "react";
+import { FilmData, NarrationPath } from "../../types/videosAndFilms";
+import Title from "./Title";
+import NarrativesLegend from "@/components/NarrativesLegend";
+
+export { NarrativesOverview };
+
+const NarrativesOverview: FC<{
+  narrativesCollection: NarrationPath[];
+}> = ({ narrativesCollection }) => {
+  const [films, setFilms] = useState<FilmData[] | null>(null);
+  // const [nowPlaying, setNowPlaying] = useState<string | null>(null);
+
+  return (
+    <>
+      <Title
+        text="Non-linear narrations"
+        theme="dark"
+        color="yellow_secondary"
+        alt={true}
+      />
+      {narrativesCollection ? (
+        <>
+          <NarrativesLegend />
+          {/*<NarrativesList />*/}
+        </>
+      ) : (
+        <div className="h-full, bg-black_bg"></div>
+      )}
+    </>
+  );
+};
