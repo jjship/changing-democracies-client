dd3726a596cc4fd81809c134f7106c6acd2b7924
ImperatorZurg <blehableha111@gmail.com>
2024-12-14 12:53:09 +0100

    chore: add additional paths for tests

---
+++ app/narratives/fifthPathMOCK.ts
@@ -0,0 +1,191 @@
+import { NarrationPath } from "../../types/videosAndFilms";
+
+export const narrationPath: NarrationPath = {
+  id: "secondId",
+  title: "What influences you in life?",
+  total_length: 1261,
+  fragments: [
+    {
+      guid: "23be7b7b-f356-4abd-b71b-602e546eb999",
+      title: "CD_LITHUANIA_Juozas Malickas_Quote 6.mp4",
+      length: 53,
+      tags: ["LITHUANIA", "JOUZAS MALICKAS"],
+      person: "Jouzas Malickas",
+      country: "LITHUANIA",
+      playerUrl:
+        "https://iframe.mediadelivery.net/play/239326/23be7b7b-f356-4abd-b71b-602e546eb999?autoplay=false",
+      thumbnailUrl:
+        "https://vz-cac74041-8b3.b-cdn.net/23be7b7b-f356-4abd-b71b-602e546eb999/thumbnail.jpg",
+      sequence: 1,
+      otherPaths: [],
+    },
+    {
+      guid: "1c29fa2a-27c9-4470-b854-e49d54a2405d",
+      title: "CD_LITHUANIA_Juozas Malickas_Quote 7.mp4",
+      length: 29,
+      tags: [],
+      person: "Jouzas Malickas",
+      country: "LITHUANIA",
+      playerUrl:
+        "https://iframe.mediadelivery.net/play/239326/1c29fa2a-27c9-4470-b854-e49d54a2405d?autoplay=false",
+      thumbnailUrl:
+        "https://vz-cac74041-8b3.b-cdn.net/1c29fa2a-27c9-4470-b854-e49d54a2405d/thumbnail.jpg",
+      sequence: 2,
+      otherPaths: [],
+    },
+    {
+      guid: "f5ff8387-a326-43c1-bedd-cf6ece252c0f",
+      title: "CD_BELGIUM_Lisbeth Ruiz Sanchez_Quote 8.mp4",
+      length: 113,
+      tags: ["BELGIUM", "LISBETH_RUIZSANCHEZ", "TRAVELLING_WORKSHOP "],
+      person: "Lisbeth Ruiz Sanchez",
+      country: "BELGIUM",
+      playerUrl:
+        "https://iframe.mediadelivery.net/play/239326/f5ff8387-a326-43c1-bedd-cf6ece252c0f?autoplay=false",
+      thumbnailUrl:
+        "https://vz-cac74041-8b3.b-cdn.net/f5ff8387-a326-43c1-bedd-cf6ece252c0f/thumbnail.jpg",
+      sequence: 3,
+      otherPaths: [],
+    },
+    {
+      guid: "3174e9aa-590d-4935-a8e6-0bb0d1df03bd",
+      title: "CD_GREECE_Petros Pizanias_Quote 5.mp4",
+      length: 108,
+      tags: ["GREECE", "PETROS_PIZANIAS", "TRAVELLING_WORKSHOP"],
+      person: "Petros Pizanias",
+      country: "GREECE",
+      playerUrl:
+        "https://iframe.mediadelivery.net/play/239326/3174e9aa-590d-4935-a8e6-0bb0d1df03bd?autoplay=false",
+      thumbnailUrl:
+        "https://vz-cac74041-8b3.b-cdn.net/3174e9aa-590d-4935-a8e6-0bb0d1df03bd/thumbnail.jpg",
+      sequence: 4,
+      otherPaths: [],
+    },
+    {
+      guid: "29c2666e-4637-4d0d-85e8-6c6b4f604686",
+      title: "CD_ROMANIA_Ivan Florian_Quote 2.mp4",
+      length: 118,
+      tags: ["ROMANIA", "IVAN FLORIAN", "TRAVELLING_WORKSHOP"],
+      person: "Ivan Florian",
+      country: "ROMANIA",
+      playerUrl:
+        "https://iframe.mediadelivery.net/play/239326/29c2666e-4637-4d0d-85e8-6c6b4f604686?autoplay=false",
+      thumbnailUrl:
+        "https://vz-cac74041-8b3.b-cdn.net/29c2666e-4637-4d0d-85e8-6c6b4f604686/thumbnail.jpg",
+      sequence: 5,
+      otherPaths: [],
+    },
+    {
+      guid: "bec228a9-df76-4c00-a393-35cd1838ab1e",
+      title: "CD_PORTUGAL_Armandina Soares_Quote 3.mp4",
+      length: 118,
+      tags: ["PORTUGAL", "ARMANDINA SOARES", "TRAVELLING_WORKSHOP"],
+      person: "Armandina Soares",
+      country: "PORTUGAL",
+      playerUrl:
+        "https://iframe.mediadelivery.net/play/239326/bec228a9-df76-4c00-a393-35cd1838ab1e?autoplay=false",
+      thumbnailUrl:
+        "https://vz-cac74041-8b3.b-cdn.net/bec228a9-df76-4c00-a393-35cd1838ab1e/thumbnail.jpg",
+      sequence: 6,
+      otherPaths: [],
+    },
+    {
+      guid: "0ad1fb9a-8034-461c-a5d6-477afe16a861",
+      title: "CD_LITHUANIA_Vladimir Davydov_Quote 3.mp4",
+      length: 233,
+      tags: ["LITHUANIA", "VLADIMIR DAVYDOV", "TRAVELLING_WORKSHOP"],
+      person: "Vladimir Davydov",
+      country: "LITHUANIA",
+      playerUrl:
+        "https://iframe.mediadelivery.net/play/239326/0ad1fb9a-8034-461c-a5d6-477afe16a861?autoplay=false",
+      thumbnailUrl:
+        "https://vz-cac74041-8b3.b-cdn.net/0ad1fb9a-8034-461c-a5d6-477afe16a861/thumbnail.jpg",
+      sequence: 7,
+      otherPaths: [],
+    },
+    {
+      guid: "20f7c4b0-c534-4e1b-87d4-e7545a860e25",
+      title: "CD_LITHUANIA_Vladimir Davydov_Quote 4.mp4",
+      length: 18,
+      tags: ["LITHUANIA", "VLADIMIR DAVYDOV", "TRAVELLING_WORKSHOP"],
+      person: "Vladimir Davydov",
+      country: "LITHUANIA",
+      playerUrl:
+        "https://iframe.mediadelivery.net/play/239326/20f7c4b0-c534-4e1b-87d4-e7545a860e25?autoplay=false",
+      thumbnailUrl:
+        "https://vz-cac74041-8b3.b-cdn.net/20f7c4b0-c534-4e1b-87d4-e7545a860e25/thumbnail.jpg",
+      sequence: 8,
+      otherPaths: [],
+    },
+    {
+      guid: "d7444420-c1ba-4ba5-b3e6-3055aa3c9e25",
+      title: "CD_NETHERLANDS_Amir Mohammadi_Quote 5.mp4",
+      length: 34,
+      tags: ["NETHERLANDS", "AMIR MOHAMMADI", "TRAVELLING_WORKSHOP"],
+      person: "Amir Mohammadi",
+      country: "NETHERLANDS",
+      playerUrl:
+        "https://iframe.mediadelivery.net/play/239326/d7444420-c1ba-4ba5-b3e6-3055aa3c9e25?autoplay=false",
+      thumbnailUrl:
+        "https://vz-cac74041-8b3.b-cdn.net/d7444420-c1ba-4ba5-b3e6-3055aa3c9e25/thumbnail.jpg",
+      sequence: 9,
+      otherPaths: [],
+    },
+    {
+      guid: "589e627d-5df1-4022-a7c3-588a7528493a",
+      title: "CD_PORTUGAL_Milice Ribeiro Dos Santos_Quote 5.mp4",
+      length: 89,
+      tags: ["PORTUGAL", "MILICE_RIBEIRO", "TRAVELLING_WORKSHOP"],
+      person: "Milice Ribeiro Dos Santos",
+      country: "PORTUGAL",
+      playerUrl:
+        "https://iframe.mediadelivery.net/play/239326/589e627d-5df1-4022-a7c3-588a7528493a?autoplay=false",
+      thumbnailUrl:
+        "https://vz-cac74041-8b3.b-cdn.net/589e627d-5df1-4022-a7c3-588a7528493a/thumbnail.jpg",
+      sequence: 10,
+      otherPaths: [],
+    },
+    {
+      guid: "8f30764c-d040-4b76-bc1b-ba1ce8afe9aa",
+      title: "CD_CZECH REPUBLIC_Michaela Bedrníková_Quote 5.mp4",
+      length: 44,
+      tags: ["CHECH REPUBLIC", "MICHAELA BEDRNIKOVA", "TRAVELLING_WORKSHOP"],
+      person: "Michaela Bedrníková",
+      country: "CHECH REPUBLIC",
+      playerUrl:
+        "https://iframe.mediadelivery.net/play/239326/8f30764c-d040-4b76-bc1b-ba1ce8afe9aa?autoplay=false",
+      thumbnailUrl:
+        "https://vz-cac74041-8b3.b-cdn.net/8f30764c-d040-4b76-bc1b-ba1ce8afe9aa/thumbnail.jpg",
+      sequence: 11,
+      otherPaths: [],
+    },
+    {
+      guid: "4ad98e79-1739-4165-ac39-ce63f4843554",
+      title: "CD_SPAIN_Andrés Ruiz Grima_Quote 11.mp4",
+      length: 154,
+      tags: ["SPAIN", "ANDRES RUIZ GRIMA"],
+      person: "Andrés Ruiz Grima",
+      country: "SPAIN",
+      playerUrl:
+        "https://iframe.mediadelivery.net/play/239326/4ad98e79-1739-4165-ac39-ce63f4843554?autoplay=false",
+      thumbnailUrl:
+        "https://vz-cac74041-8b3.b-cdn.net/4ad98e79-1739-4165-ac39-ce63f4843554/thumbnail.jpg",
+      sequence: 12,
+      otherPaths: [],
+    },
+    {
+      guid: "487229fe-7d4c-4fe3-8c80-90cee9f5b85e",
+      title: "CD_NETHERLANDS_Chee-Han Kartosen-Wong_Quote 5.mp4",
+      length: 150,
+      tags: ["NETHERLANDS", "CHEE-HAN KARTOSEN-WONG"],
+      person: "Chee-Han Kartosen-Wong",
+      country: "NETHERLANDS",
+      playerUrl:
+        "https://iframe.mediadelivery.net/play/239326/487229fe-7d4c-4fe3-8c80-90cee9f5b85e?autoplay=false",
+      thumbnailUrl:
+        "https://vz-cac74041-8b3.b-cdn.net/487229fe-7d4c-4fe3-8c80-90cee9f5b85e/thumbnail.jpg",
+      sequence: 13,
+      otherPaths: [],
+    },
+  ],
+};
