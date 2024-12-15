4d195f80121a0a88c32a800a47fda71eafcb551b
Jacek Skrzypek <devontheroof@gmail.com>
2024-12-15 01:50:08 +0100

    narration description as array

--- app/components/NarrativesList.tsx
+++ app/components/NarrativesList.tsx
@@ -54,7 +54,13 @@ const NarrativesList: React.FC<{
             </div>
           </Box>
           <Box className="flex flex-1 flex-col justify-center">
-            <p className="text-white">{narration.description}</p>
+            {narration.description?.length ? (
+              narration.description.map((line) => (
+                <p className="text-white">{line}</p>
+              ))
+            ) : (
+              <p className="text-white">""</p>
+            )}
             <div className="relative mt-8 flex w-1/2 flex-row items-center">
               <Image
                 className="absolute"
