0bf2051c145b12ba0ebdcc5299da12166e85a7e8
ImperatorZurg <blehableha111@gmail.com>
2025-01-08 14:20:20 +0100

    fix: remove unnecessary function passing through props. use useEffect to prevent rendering layout and countdown in same time error

--- app/components/narrations/CountDown.tsx
+++ app/components/narrations/CountDown.tsx
@@ -1,15 +1,12 @@
-"use client";
-
 import React, { FC, useEffect, useState } from "react";
+import { useNarrationContext } from "@/app/narratives/NarrationsContext";
 
-export { CountDown };
-
-const CountDown: FC<{
-  onFinish: () => void;
-}> = ({ onFinish }) => {
+const CountDown: FC = () => {
   const [count, setCount] = useState<number>(5);
   const radius = 40;
   const circumference = 2 * Math.PI * radius;
+
+  const { setIsPlaying } = useNarrationContext();
 
   const offset = circumference - ((count - 1) / 5) * circumference;
 
@@ -18,7 +15,6 @@ const CountDown: FC<{
       setCount((prevCount) => {
         if (prevCount <= 1) {
           clearInterval(timer);
-          onFinish();
           return 0;
         }
         return prevCount - 1;
@@ -26,45 +22,51 @@ const CountDown: FC<{
     }, 1000);
 
     return () => clearInterval(timer);
-  }, [onFinish]);
+  }, []);
+
+  useEffect(() => {
+    if (count === 0) {
+      setIsPlaying(true);
+    }
+  }, [count, setIsPlaying]);
 
   return (
-    <>
-      <div className="max-h-xs relative flex h-full w-full max-w-xs items-center justify-center">
-        <svg viewBox="0 0 100 100" className="h-1/2 w-1/2">
-          <circle
-            stroke="rgba(255, 0, 0, 0.2)"
-            fill="transparent"
-            strokeWidth="8"
-            r={radius}
-            cx="50"
-            cy="50"
-          />
-          <circle
-            stroke="rgba(184,82,82)"
-            fill="transparent"
-            strokeWidth="8"
-            r={radius}
-            cx="50"
-            cy="50"
-            strokeDasharray={circumference}
-            strokeDashoffset={offset}
-            className="transition-all duration-1000 ease-in-out"
-          />
-          <circle
-            stroke="rgba(184,82,82)"
-            fill="transparent"
-            strokeWidth="8"
-            r={radius}
-            cx="50"
-            cy="50"
-            strokeDasharray="5, 5"
-            strokeDashoffset={offset}
-            className="transition-all duration-1000 ease-in-out"
-          />
-        </svg>
-        <span className="absolute text-4xl text-red_mains">{count}</span>
-      </div>
-    </>
+    <div className="max-h-xs relative flex h-full w-full max-w-xs items-center justify-center">
+      <svg viewBox="0 0 100 100" className="h-1/2 w-1/2">
+        <circle
+          stroke="rgba(255, 0, 0, 0.2)"
+          fill="transparent"
+          strokeWidth="8"
+          r={radius}
+          cx="50"
+          cy="50"
+        />
+        <circle
+          stroke="rgba(184,82,82)"
+          fill="transparent"
+          strokeWidth="8"
+          r={radius}
+          cx="50"
+          cy="50"
+          strokeDasharray={circumference}
+          strokeDashoffset={offset}
+          className="transition-all duration-1000 ease-in-out"
+        />
+        <circle
+          stroke="rgba(184,82,82)"
+          fill="transparent"
+          strokeWidth="8"
+          r={radius}
+          cx="50"
+          cy="50"
+          strokeDasharray="5, 5"
+          strokeDashoffset={offset}
+          className="transition-all duration-1000 ease-in-out"
+        />
+      </svg>
+      <span className="absolute text-4xl text-red_mains">{count}</span>
+    </div>
   );
 };
+
+export { CountDown };
