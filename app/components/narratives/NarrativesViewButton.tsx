13b6fc2f9b5f3727c7555b59637b1a949d6d762b
ImperatorZurg <blehableha111@gmail.com>
2025-01-11 22:39:17 +0200

    fix: make continue/start button thinner and smaller (as in project)

--- app/components/narratives/NarrativesViewButton.tsx
+++ app/components/narratives/NarrativesViewButton.tsx
@@ -29,7 +29,7 @@ const NarrativesViewButton: FC<NarrativesButtonProps> = ({
             ? "70px 100px 100px 70px"
             : "100px 70px 70px 100px",
         }}
-        className={`relative flex h-[7vw] w-[25vw] items-center space-x-2 overflow-visible ${
+        className={`relative flex h-[5vw] w-[20vw] items-center space-x-2 overflow-visible ${
           isRightPlacement ? "rounded-r-full" : "rounded-l-full"
         } bg-[#b85252] px-10 py-2 text-white opacity-90 shadow-lg`}
       >
@@ -41,9 +41,9 @@ const NarrativesViewButton: FC<NarrativesButtonProps> = ({
           }}
           className={`absolute ${
             isRightPlacement
-              ? "right-[80%] border-r-[10vw]"
-              : "left-[80%] border-l-[10vw]"
-          } h-0 w-full border-b-[6vw] border-t-[6vw] border-transparent`}
+              ? "right-[80%] border-r-[8vw]"
+              : "left-[80%] border-l-[8vw]"
+          } h-0 w-full border-b-[5vw] border-t-[5vw] border-transparent`}
         ></Box>
         <Text className="w-full text-center text-[2vw] font-bold">{text}</Text>
       </button>
