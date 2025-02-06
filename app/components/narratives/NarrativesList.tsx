f3c3d1dde591be938555193bb0e7bb238dd09acc
Jacek Skrzypek <devontheroof@gmail.com>
2025-02-06 09:48:01 +0100

    fix narr images

--- app/components/narratives/NarrativesList.tsx
+++ app/components/narratives/NarrativesList.tsx
@@ -6,7 +6,18 @@ export { NarrativesList };
 
 const NarrativesList: FC = () => {
   const [isClient, setIsClient] = useState(false);
-  const images = ["q1.png", "q2.png", "q3.png", "q4.png", "q5.png"];
+  const images = [
+    "q1.png",
+    "q2.png",
+    "q3.png",
+    "q4.png",
+    "q5.png",
+    "q6.png",
+    "q7.png",
+    "q8.png",
+    "q9.png",
+    "q10.png",
+  ];
   const { narrationPaths, setCurrentPath } = useNarrativesContext();
 
   useEffect(() => {
