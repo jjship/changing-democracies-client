30df812cd52cbd6b89f038e53a844bb3000a5fcb
Jacek Skrzypek <devontheroof@gmail.com>
2025-02-07 09:42:43 +0100

    fix font sizes

--- app/scroll-documentary/slides/slide_15.tsx
+++ app/scroll-documentary/slides/slide_15.tsx
@@ -5,15 +5,19 @@ const archivoNarrow = Archivo_Narrow({ subsets: ["latin"] });
 export default function Slide15Content() {
   return (
     <div className="relative right-1/4 flex h-full w-[24rem] flex-col items-center justify-center">
-      <div className="text-sm text-purple_lightest_bg ">
-        <h2 className="mb-4 text-2xl font-bold text-yellow_secondary ">
+      <div style={{ fontSize: "1vw" }} className=" text-purple_lightest_bg ">
+        <h2
+          style={{ fontSize: "2vw" }}
+          className="mb-4  font-bold text-yellow_secondary "
+        >
           Credits
         </h2>
         <p className="mb-2">
           <span className="font-bold text-darkRed">Changing Democracies</span>{" "}
-          aims to explore how Europe’s living history about recent transitions
-          to democracy might help us to explore and grasp what processes are
-          needed today for democracy to fulfil its promises for everyone.
+          aims to explore how Europe&ldquo;s living history about recent
+          transitions to democracy might help us to explore and grasp what
+          processes are needed today for democracy to fulfil its promises for
+          everyone.
         </p>
         <p className="mb-8">
           The project is the result of a collaboration between 12 partners from
