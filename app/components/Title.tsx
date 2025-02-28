2295b2641a7fd97f57bf8118016890565084594c
Jacek Skrzypek <devontheroof@gmail.com>
2025-02-28 20:16:27 +0100

    edupack

--- app/components/Title.tsx
+++ app/components/Title.tsx
@@ -24,7 +24,7 @@ export default function Title(props: {
       className={`${bgColor} sticky ${topDistance} z-30 md:relative md:top-0 md:z-20 `}
     >
       <h1
-        className={`${fontClass} py-[0.3em] pl-5 text-[2.25rem] leading-9 tracking-[-0.064rem] md:py-10 md:pl-12 md:text-7xl md:leading-8 xl:pl-24 xl:text-[4.5rem]  ${
+        className={`${fontClass} pb-2 pl-5 text-[2.25rem] leading-9 tracking-[-0.064rem] md:py-10 md:pl-12 md:text-7xl md:leading-8 xl:pl-24 xl:text-[4.5rem]  ${
           color ? "text-" + color : ""
         }`}
       >
