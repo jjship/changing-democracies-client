b9958f7e8bb24670d150150a9c2aadc23a53fa65
Jacek Skrzypek <devontheroof@gmail.com>
2025-05-01 14:16:16 +0200

    fix contact responsiveness

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -2,18 +2,15 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import logoLight from "@/public/logo_light_no_bg.svg";
 
 import roundPhoto from "@/public/photo in circle.png";
-import Title from "./Title";
 import { useTranslation } from "../[lang]/context/TranslationContext";
 export default function Contact() {
   const { dictionary: dict } = useTranslation();
   return (
     <>
-      <Title text={dict.navigation.contact} theme="dark" />
-      <div className="relative mt-6 md:z-20 md:ml-10  xl:ml-28">
-        <div className=" absolute z-10 mr-[5.5rem] mt-[1rem] md:mr-[26rem] md:mt-[2.5rem] xl:mr-[40rem]">
+      <div className="relative md:z-20 md:ml-10  xl:ml-28">
+        <div className=" absolute z-10 mr-[5.5rem] mt-[1rem] md:mr-[30rem] md:mt-[2.5rem] xl:mr-[40rem]">
           <Image
             src={roundPhoto}
             alt="retro photo of a boy with flowers and a man on a motorbike"
@@ -22,11 +19,9 @@ export default function Contact() {
         </div>
         <div className="relative z-20 text-display_sm font-bold leading-[4.5rem] tracking-[-.03em] text-red_mains mix-blend-screen md:text-[11rem] md:leading-[12.5rem]  xl:text-[12.5rem]">
           <p className="ml-[2.875rem] md:ml-[3rem]">{dict.contact.getIn}</p>
-          <div className=" mr-4 mt-5 text-right underline mix-blend-overlay md:ml-64 md:text-left ">
-            <Link href="mailto:touch@changingdemocracies.com">
-              {dict.contact.touch}
-            </Link>
-          </div>
+          <p className=" mr-4 text-right lowercase md:ml-64 md:text-left ">
+            {dict.contact.touch}
+          </p>
         </div>
         <div className="flex w-full flex-col items-end">
           <div
@@ -48,7 +43,7 @@ export default function Contact() {
           </div>
         </div>
       </div>
-      <div className="relative z-20 mt-20 max-w-max border-solid border-white md:mx-auto md:w-full">
+      <div className="relative z-20 mt-6 max-w-max border-solid border-white md:mx-auto md:w-full xl:mt-40">
         <span className="leading-6 text-purple_lightest_bg md:text-2xl xl:text-[2.125rem] xl:leading-10">
           <Link
             href="/cypp_policy.pdf"
@@ -57,11 +52,7 @@ export default function Contact() {
           >
             {dict.contact.download}
           </Link>
-          .
         </span>
-      </div>
-      <div className="ml-6 mr-36 mt-0 md:hidden">
-        <Image src={logoLight} alt="changing democracies logo" />
       </div>
     </>
   );
