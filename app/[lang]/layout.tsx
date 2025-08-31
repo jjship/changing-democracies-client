9d8d440fa2da5c1d5e853d6c8c3c7800bc5bed11
Jacek Skrzypek <devontheroof@gmail.com>
2025-08-31 21:23:01 +0200

    fix google tag setup

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -56,23 +56,29 @@ export default function RootLayout({
     <>
       <html lang={lang} suppressHydrationWarning>
         <head>
+          {/* Google Tag Manager */}
+          <Script id="google-tag-manager" strategy="afterInteractive">
+            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P6F8FHST');`}
+          </Script>
+          {/* End Google Tag Manager */}
           <StructuredData type="organization" lang={lang} />
           <StructuredData type="website" lang={lang} />
         </head>
         <body className={`${archivo.className} bg-black_bg antialiased`}>
-          {/* Google Analytics */}
-          <Script
-            src="https://www.googletagmanager.com/gtag/js?id=G-13VS8XS26Q"
-            strategy="afterInteractive"
-          />
-          <Script id="google-analytics" strategy="afterInteractive">
-            {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-13VS8XS26Q');
-            `}
-          </Script>
+          {/* Google Tag Manager (noscript) */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-P6F8FHST"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
+          {/* End Google Tag Manager (noscript) */}
           {children}
         </body>
       </html>
