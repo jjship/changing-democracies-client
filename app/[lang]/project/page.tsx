c80d716a77e6f395de0c9d50a1f511e93c19d382
Jacek Skrzypek <devontheroof@gmail.com>
2025-10-03 10:15:31 +0200

    project page refactor with disclaimer

--- app/[lang]/project/page.tsx
+++ app/[lang]/project/page.tsx
@@ -2,25 +2,26 @@ import { LangParam } from "@/types/langParam";
 import { NavigationContainer } from "@/components/navigation/NavigationContainer";
 import { Suspense } from "react";
 import { getDictionary } from "../dictionaries";
-import { TranslationProvider } from "../context/TranslationContext";
+import { TranslationProvider } from "@/translation/TranslationContext";
 import { Section } from "@/components/Section";
-import { CDLanguages } from "@/utils/i18n/languages";
-import Project from "@/components/Project";
+import PageFooter from "../../components/PageFooter";
+import Project from "../../components/Project";
 
-export default async function ProjectPage({ params: { lang } }: LangParam) {
-  const dictionary = await getDictionary(lang as CDLanguages);
+export default async function EduResources({ params: { lang } }: LangParam) {
+  const dictionary = await getDictionary(lang);
 
   return (
     <TranslationProvider dictionary={dictionary}>
       <Suspense>
-        <div className="flex h-screen flex-col bg-purple_lightest_bg">
+        <div className="flex min-h-screen flex-col justify-between bg-purple_lightest_bg">
           <NavigationContainer
             fontColor="black_bg"
             bgColor="purple_lightest_bg"
           />
-          <Section theme="light" id="project">
+          <Section theme="light" id="edu-resources">
             <Project />
           </Section>
+          <PageFooter theme="light" />
         </div>
       </Suspense>
     </TranslationProvider>
