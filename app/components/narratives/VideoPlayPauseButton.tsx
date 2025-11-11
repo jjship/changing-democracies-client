31925caf78311a6f4cdeaf6f897f9384f6fa4805
Jacek Skrzypek <devontheroof@gmail.com>
2025-11-11 14:46:47 +0100

    fix layout

--- app/components/narratives/VideoPlayPauseButton.tsx
+++ app/components/narratives/VideoPlayPauseButton.tsx
@@ -20,7 +20,7 @@ export const VideoPlayPauseButton: FC<VideoPlayPauseButtonProps> = ({
       onClick={onToggle}
       size="icon"
       variant="secondary"
-      className="absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2 opacity-0 transition-opacity duration-200 hover:scale-110 group-hover/video:opacity-100"
+      className="pointer-events-auto absolute left-1/2 top-1/2 z-20 -translate-x-1/2 -translate-y-1/2 opacity-0 transition-opacity duration-200 hover:scale-110 group-hover/video:opacity-100"
     >
       {isPaused ? (
         <Play className="h-6 w-6" />
